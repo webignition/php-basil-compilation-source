bb38b325487d3ba5ed29d5d5e26e1729a7be4a58
Jon Cram <jon@webignition.net>
2019-10-03 13:19:33 +0100

    Add VariablePlaceholder

---
+++ tests/Unit/VariablePlaceholderTest.php
@@ -0,0 +1,43 @@
+<?php
+/** @noinspection PhpDocSignatureInspection */
+/** @noinspection PhpUnhandledExceptionInspection */
+
+declare(strict_types=1);
+
+namespace webignition\BasilCompilationSource\Tests\Unit;
+
+use webignition\BasilCompilationSource\VariablePlaceholder;
+
+class VariablePlaceholderTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @dataProvider createDataProvider
+     */
+    public function testCreate(string $name, string $id, string $expectedId)
+    {
+        $placeholder = new VariablePlaceholder($name, $id);
+
+        $this->assertEquals($expectedId, $placeholder->getId());
+    }
+
+    public function createDataProvider(): array
+    {
+        return [
+            'implicit id' => [
+                'name' => 'PLACEHOLDER',
+                'id' => '',
+                'expectedId' => 'PLACEHOLDER',
+            ],
+            'explicit id' => [
+                'name' => 'PLACEHOLDER',
+                'id' => 'ID',
+                'expectedId' => 'ID',
+            ],
+        ];
+    }
+
+    public function testToString()
+    {
+        $this->assertSame('{{ NAME }}', (string) new VariablePlaceholder('NAME'));
+    }
+}
