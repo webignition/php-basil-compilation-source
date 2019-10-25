6b13e8f73b7b413cf1c81e6005e304301c7c058f
Jon Cram <jon@webignition.net>
2019-10-25 11:31:23 +0100

    Add SourceInterface::mutateLastStatement()

--- src/StatementListInterface.php
+++ src/StatementListInterface.php
@@ -4,7 +4,6 @@ namespace webignition\BasilCompilationSource;
 
 interface StatementListInterface extends SourceInterface
 {
-    public function mutateLastStatement(callable $mutator);
     public function addClassDependenciesToLastStatement(ClassDependencyCollection $classDependencies);
     public function addVariableDependenciesToLastStatement(VariablePlaceholderCollection $variableDependencies);
     public function addVariableExportsToLastStatement(VariablePlaceholderCollection $variableExports);
