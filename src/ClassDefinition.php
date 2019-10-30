08663c5cb2e1e2d7d95848e189cdbcb92c6e1038
Jon Cram <jon@webignition.net>
2019-10-30 12:30:41 +0000

    Crate ClassDefinition

---
+++ src/ClassDefinition.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+
+namespace webignition\BasilCompilationSource;
+
+class ClassDefinition implements ClassDefinitionInterface
+{
+    private $name;
+    private $functions = [];
+
+    public function __construct(string $name, array $functions)
+    {
+        $this->name = $name;
+
+        foreach ($functions as $function) {
+            if ($function instanceof FunctionDefinitionInterface) {
+                $this->functions[] = $function;
+            }
+        }
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return FunctionDefinitionInterface[]
+     */
+    public function getFunctions(): array
+    {
+        return $this->functions;
+    }
+
+    public function getMetadata(): MetadataInterface
+    {
+        $metadata = new Metadata();
+
+        foreach ($this->functions as $function) {
+            if ($function instanceof FunctionDefinitionInterface) {
+                $metadata->add($function->getMetadata());
+            }
+        }
+
+        return $metadata;
+    }
+
+    public function jsonSerialize(): array
+    {
+        $functions = [];
+
+        foreach ($this->functions as $function) {
+            if ($function instanceof FunctionDefinitionInterface) {
+                $functions[] = $function->jsonSerialize();
+            }
+        }
+
+        return [
+            'type' => 'class',
+            'name' => $this->name,
+            'functions' => $functions,
+        ];
+    }
+
+    /**
+     * @return SourceInterface[]
+     */
+    public function getSources(): array
+    {
+        return $this->getFunctions();
+    }
+}
