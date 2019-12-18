2897719d3dc2aa9864d7861d0dba8f5654451fbb
Jon Cram <jon@webignition.net>
2019-12-18 10:44:50 +0000

    Remove phpstan 'checkGenericClassInNonGenericObjectType: false'

--- src/VariablePlaceholderCollection.php
+++ src/VariablePlaceholderCollection.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace webignition\BasilCompilationSource;
 
+/**
+ * @implements \Iterator<VariablePlaceholder>
+ */
 class VariablePlaceholderCollection implements \Iterator, \Countable
 {
     /**
